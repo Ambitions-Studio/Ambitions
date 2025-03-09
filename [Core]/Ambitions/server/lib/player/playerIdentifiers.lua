6eedadf8c420dd46330d175d6f95eb938e6aeab1
Primordial-Devv <153821912+Primordial-Devv@users.noreply.github.com>
2025-03-09 16:40:31 +0100

    Update playerIdentifiers.lua

--- [Core]/Ambitions/server/lib/player/playerIdentifiers.lua
+++ [Core]/Ambitions/server/lib/player/playerIdentifiers.lua
@@ -1,23 +1,14 @@
 --- Retrieve the identifiers for a player.
 --- @param source number The player's server ID.
---- @return PlayerIdentifiers|nil identifiers Table containing all player identifiers, or nil if player is not found.
+--- @return table identifiers Table containing all player identifiers, or nil if player is not found.
 function ABT.Player.GetPlayerIdentifier(source)
     local identifiers = {}
-<<<<<<< HEAD
-    identifiers['name'] = GetPlayerName(source)
-    for k,v in pairs(GetPlayerIdentifiers(source)) do
-        identifiers[v:match("^(%w+)")] = v:match(":(.+)$")
-    end
-
-=======
 
     identifiers['name'] = GetPlayerName(source)
     for _,v in pairs(GetPlayerIdentifiers(source)) do
         identifiers[v:match("^(%w+)")] = v:match(":(.+)$")
     end
 
-    ABT.Print.Log("Player identifiers: ", identifiers)
->>>>>>> fc868f72a2052e1ed94c1ea04fc0759cc09fe045
     return identifiers
 end
 
