a1452664ac28ea140acab42424d8543d2430082a
White Wolf | Henry <140905559+WWolf-dev@users.noreply.github.com>
2025-06-01 23:07:09 +0200

    FEAT - Adding importation system to load module
    Implements a complete custom importation system to load modules, tables, or functions across resources.
    Lays the foundation for standardized modular development within the Ambitions framework.

---
+++ fxmanifest.lua
@@ -0,0 +1,48 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Ambitions Studio'
+
+description 'Ambitions — A modern, scalable, and secure FiveM framework built with Lua 5.4 standards. Designed for performance, modularity, and full open-source collaboration.'
+
+version '0.0.0'
+
+name 'Ambitions'
+
+lua54 'yes'
+
+shared_script 'importation.lua'
+
+shared_scripts {
+    'shared/initExports.lua',
+
+    'shared/lib/print/log.lua',
+
+    'translations/*.lua',
+
+    'config/shared/*.lua',
+    'shared/lib/**/*.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+
+    'server/initExports.lua',
+
+    'config/server/*.lua',
+    'server/lib/**/*.lua',
+}
+
+client_scripts {
+    'client/lib/**/*.lua',
+}
+
+files {
+    'init.lua',
+    'importation.lua',
+}
+
+dependencies {
+    'oxmysql'
+}
