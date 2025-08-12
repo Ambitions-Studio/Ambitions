bf672b3a643909594f538d2cba8297a28fcab4d7
Primordial-Devv <primordial.devv@gmail.com>
2025-08-12 21:12:21 +0200

    FEAT - Testing identifiers player

--- server/modules/core/saveSpawn.lua
+++ server/modules/core/saveSpawn.lua
@@ -1,4 +1,5 @@
 local ambitionsPrint = require('lib.log.ambition-print')
+local identifiers = require('server.lib.player.identifiers')
 
 local function FirstSpawn()
 
@@ -9,7 +10,8 @@ local function RegularSpawn()
 end
 
 local function CheckFirstSpawn()
-
+  local playerIdentifiers = identifiers.get(source).getAll()
+  ambitionsPrint.debug('Player identifiers: ' .. playerIdentifiers)
 end
 
 
