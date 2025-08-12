1c3e0fdf3030c75db6630ce42878b68f59006da1
Primordial-Devv <primordial.devv@gmail.com>
2025-08-12 21:52:55 +0200

    FEAT - Starting the check first spawn

--- server/modules/core/saveSpawn.lua
+++ server/modules/core/saveSpawn.lua
@@ -1,4 +1,4 @@
-local ambitionsPrint = require('lib.log.ambition-print')
+local ambitionsPrint = require('shared.lib.log.print')
 local identifiers = require('server.lib.player.identifiers')
 
 local function FirstSpawn()
@@ -10,8 +10,24 @@ local function RegularSpawn()
 end
 
 local function CheckFirstSpawn()
-  local playerIdentifiers = identifiers.get(source).getAll()
-  ambitionsPrint.debug('Player identifiers: ' .. playerIdentifiers)
+  local SESSION_ID <const> = source
+  local PLAYER_IDENTIFIERS <const> = identifiers.get(SESSION_ID)
+  local PLAYER_LICENSE <const> = PLAYER_IDENTIFIERS.license
+
+  if not PLAYER_LICENSE then
+    ambitionsPrint.error('Failed to get player license for session id : ', SESSION_ID)
+    DropPlayer(SESSION_ID, 'Failed to get your FiveM license, please contact an administrator.')
+    return
+  end
+
+  MySQL.Async.fetchScalar('SELECT id FROM users WHERE license = ?', { PLAYER_LICENSE }, function(result)
+    if not result then
+      ambitionsPrint.error('Failed to get User Id for license: ', PLAYER_LICENSE)
+      DropPlayer(SESSION_ID, 'Failed to get your user id, please contact an administrator.')
+      return
+    end
+    ambitionsPrint.debug('User Id for license: ', PLAYER_LICENSE, ' is: ', result)
+  end)
 end
 
 
