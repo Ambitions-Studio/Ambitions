33044a9803cc306aed9308297672b64f4ec23847
Primordial-Devv <primordial.devv@gmail.com>
2025-08-12 20:33:20 +0200

    FIX - Fixinginit server

--- server/init.lua
+++ server/init.lua
@@ -1,5 +1,5 @@
 local ambitionsPrint = require('shared.lib.log.print')
 
 MySQL.ready(function()
-  ambitionsPrint.debug(('^6[%s - %s] ^5 - Framework initialized'):format(GetCurrentResourceName(), GetCurrentResourceName()))
+  ambitionsPrint.success(('^6[%s - %s] ^5 - Framework initialized'):format(GetInvokingResource():upper(), GetCurrentResourceName()))
 end)
