965b53528f4361fc9d5f6c0c7a02a05171f5b37b
Primordial-Devv <primordial.devv@gmail.com>
2025-08-15 17:39:03 +0200

    FIX - Fixing issue while connecting

--- server/modules/core/savePlayer.lua
+++ server/modules/core/savePlayer.lua
@@ -1,5 +1,5 @@
 local ambitionsPrint = require('shared.lib.log.print')
 
-AddEventHandler('playerDropped', function(reason; resourceName, clientDropReason)
+AddEventHandler('playerDropped', function(reason, resourceName, clientDropReason)
   ambitionsPrint.debug('Player ', GetPlayerName(source), ' dropped ( Reason : ', reason, ' Resource : ', resourceName, ' Client Drop Reason : ', clientDropReason, '.')
 end)
