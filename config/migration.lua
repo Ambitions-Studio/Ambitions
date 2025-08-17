593fadc17c692c61e8a15a7f83bf2aff9fe4c43a
Primordial-Devv <153821912+Primordial-Devv@users.noreply.github.com>
2025-08-17 14:53:17 +0200

    Feature/auto migration (#15)
    * FEAT - Adding full auto migration system for the framework sql
    
    * FIX - Fixing missing line for auto migration
    
    * FIX - Fixing issue with local function
    
    * Fix issue
    
    * FIX - Fixing order of auto migration
    
    * FIX - Fixing issues

---
+++ config/migration.lua
@@ -0,0 +1,5 @@
+return {
+  enabled = true,          -- Set to false to disable auto-migration
+  runOnStart = true,       -- Run migration when resource starts
+  logLevel = 'info'        -- 'debug', 'info', 'warning', 'error'
+}
