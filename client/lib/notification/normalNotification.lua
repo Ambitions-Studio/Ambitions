d276f4b92dabdd3cc2ed608b0e51a3684d8a8364
Primordial-Devv <primordial.devv@gmail.com>
2025-11-16 19:35:19 +0100

    FEAT - Adding abastraction for the notification system

---
+++ client/lib/notification/normalNotification.lua
@@ -0,0 +1,12 @@
+--- Displays a notification to the player using the Ambitions-Notify system
+---@param notificationTitle string The title of the notification
+---@param notificationMessage string|false The description/message of the notification, or false for no description
+---@param notificationType string The type of notification: 'success', 'error', 'info', 'warning', or 'debug'
+---@param notificationDuration number The duration in milliseconds the notification should be displayed
+---@param notificationPosition string The position on screen: 'top-left', 'top-center', 'top-right', 'middle-left', 'middle-center', 'middle-right', 'bottom-left', 'bottom-center', 'bottom-right'
+---@return void
+function amb.ShowNotification(notificationTitle, notificationMessage, notificationType, notificationDuration, notificationPosition)
+    if GetResourceState('Ambitions-Notify') ~= 'missing' then
+        return exports['Ambitions-Notify']:Notify(notificationTitle, notificationMessage, notificationType, notificationDuration, notificationPosition)
+    end
+end
