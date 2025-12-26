43b7cec656baccd870f010666ce7235c931438f7
Primordial-Devv <primordial.devv@gmail.com>
2025-12-26 01:34:29 +0100

    FIX - Fixing issues with swap item et merge item

--- server/modules/inventory/events.lua
+++ server/modules/inventory/events.lua
@@ -1,3 +1,63 @@
+RegisterNetEvent('Ambitions:inventory:swapSlots', function(fromSlot, toSlot)
+    local sessionId = source
+    if not sessionId or not fromSlot or not toSlot then
+        return
+    end
+
+    local player = amb.cache.getPlayer(sessionId)
+    if not player then
+        return
+    end
+
+    local character = player.getCurrentCharacter()
+    if not character then
+        return
+    end
+
+    local inventoryManager = character.getInventoryManager()
+    if not inventoryManager then
+        return
+    end
+
+    local items = inventoryManager.getItems()
+
+    local temp = items[fromSlot]
+    items[fromSlot] = items[toSlot]
+    items[toSlot] = temp
+end)
+
+RegisterNetEvent('Ambitions:inventory:mergeItems', function(fromSlot, toSlot)
+    local sessionId = source
+    if not sessionId or not fromSlot or not toSlot then
+        return
+    end
+
+    local player = amb.cache.getPlayer(sessionId)
+    if not player then
+        return
+    end
+
+    local character = player.getCurrentCharacter()
+    if not character then
+        return
+    end
+
+    local inventoryManager = character.getInventoryManager()
+    if not inventoryManager then
+        return
+    end
+
+    local items = inventoryManager.getItems()
+
+    local fromItem = items[fromSlot]
+    local toItem = items[toSlot]
+
+    if fromItem and toItem and fromItem.name == toItem.name then
+        toItem.count = toItem.count + fromItem.count
+        items[fromSlot] = nil
+    end
+end)
+
 AddEventHandler('Ambitions:inventory:applyChanges', function(sessionId, changes)
     if not sessionId or not changes then
         return
